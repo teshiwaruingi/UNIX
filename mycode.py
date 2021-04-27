2e848d76d263e4d6de081f2d742454367bd10c8a
Teshi Waruingi <71100831+teshiwaruingi@users.noreply.github.com>
2021-04-27 00:05:46 -0400

    Add files via upload

---
+++ mycode.py
@@ -0,0 +1,7 @@
+def change(n,c):
+
+    ''.join([ascii_letters[(ascii_letters.index(c)+n)%len(ascii_letters)] 
+             if c in ascii_letters else c for c in s])
+
+    " ".join(splitAt(x,4))
+    
